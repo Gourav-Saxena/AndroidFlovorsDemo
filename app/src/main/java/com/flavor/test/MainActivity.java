a03e9d95e8326613dd2f2a0a5476fa82368fb2d2
rajatkumar001 <rajat.kumar@eastwestbank.com>
2018-11-18 02:22:42 +0530

    Fetch Url from Favor

--- app/src/main/java/com/flavor/test/MainActivity.java
+++ app/src/main/java/com/flavor/test/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import static com.flavor.test.BuildConfig.FLAVOR;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView)findViewById(R.id.demoTest)).setText(BuildConfig.FLAVOR);
+        ((TextView)findViewById(R.id.demoTest)).setText("Build Type = "+BuildConfig.BUILD_TYPE +" FLAVOR= "+ FLAVOR +" = "+ "URL=>  "+BuildConfig.URL_BASE );
     }
 }
